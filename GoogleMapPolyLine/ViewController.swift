8ee900fa0e079b5cc45c36754b47e9311fa53a50
Enamul Haque <ehaque95@gmail.com>
2019-12-01 21:11:51 +0600

    update

--- GoogleMapPolyLine/ViewController.swift
+++ GoogleMapPolyLine/ViewController.swift
@@ -9,94 +9,51 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
-class ViewController: UIViewController {
+class ViewController: UIViewController ,GMSMapViewDelegate{
 //GMSMapViewDelegate
     @IBOutlet weak var mapView: GMSMapView!
     let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
        
-    }
-    override func loadView() {
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-              let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-              view = mapView
         
-    // Create a rectangular path
-    let rect = GMSMutablePath()
-    rect.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.0))
-    rect.add(CLLocationCoordinate2D(latitude: 37.45, longitude: -122.0))
-    rect.add(CLLocationCoordinate2D(latitude: 37.45, longitude: -122.2))
-    rect.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.2))
-
-    // Create the polygon, and assign it to the map.
-    let polygon = GMSPolygon(path: rect)
-    polygon.fillColor = UIColor(red: 0.25, green: 0, blue: 0, alpha: 0.05);
-    polygon.strokeColor = .black
-    polygon.strokeWidth = 2
-    polygon.map = mapView
-}
-    
-    
-    
-     func loadView1() {
-       // Create a GMSCameraPosition that tells the map to display the
-       // coordinate -33.86,151.20 at zoom level 6.
-       let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-       let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-       view = mapView
-
-       // Creates a marker in the center of the map.
-//       let marker = GMSMarker()
-//       marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-//       marker.title = "Sydney"
-//       marker.snippet = "Australia"
-//       marker.map = mapView
-        
-        let path = GMSMutablePath()
-        path.addLatitude(-37.81319, longitude: 144.96298)
-        path.addLatitude(-31.95285, longitude: 115.85734)
-
-        let polyline = GMSPolyline(path: path)
-        polyline.strokeWidth = 10.0
-        polyline.geodesic = true
-        polyline.map = mapView
+        let camera = GMSCameraPosition.camera(withLatitude: 37.4, longitude:-122.0, zoom: 12)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+//
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 37.36, longitude: -122.0)
+        marker.title = "Cyberjaya"
+        marker.snippet = "Malaysia"
+        marker.map = mapView
         
         
-        
-//       let circleCenter = CLLocationCoordinate2D(latitude: 37.35, longitude: -122.0)
-//       let circ = GMSCircle(position: circleCenter, radius: 1000)
-//       circ.map = mapView
-     }
+        // Create a rectangular path
+        let rect = GMSMutablePath()
+        rect.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.0))
+        rect.add(CLLocationCoordinate2D(latitude: 37.45, longitude: -122.0))
+        rect.add(CLLocationCoordinate2D(latitude: 37.45, longitude: -122.2))
+        rect.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.2))
 
-
-    func drawRectange(){
-   let path = GMSMutablePath()
-   path.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.0))
-   path.add(CLLocationCoordinate2D(latitude: 37.45, longitude: -122.0))
-   path.add(CLLocationCoordinate2D(latitude: 37.45, longitude: -122.2))
-   path.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.2))
-   path.add(CLLocationCoordinate2D(latitude: 37.36, longitude: -122.0))
-
-//   let polyLine = GMSPolyline(path: path)
-//         polyLine.isTappable = true
-//      polyLine.strokeColor = .black
-//         polyLine.strokeWidth = 10.0
-//     polyLine.map = mapView
-//        mapView.settings.myLocationButton = true
-        
-        let polygon = GMSPolygon(path: path)
+        // Create the polygon, and assign it to the map.
+        let polygon = GMSPolygon(path: rect)
         polygon.fillColor = UIColor(red: 0.25, green: 0, blue: 0, alpha: 0.05);
-        polygon.strokeColor = .black
-        polygon.strokeWidth = 10
+        polygon.strokeColor = UIColor.init(hue: 210, saturation: 88, brightness: 84, alpha: 1)
+        //polygon.strokeColor = .black
+        polygon.strokeWidth = 2
         polygon.map = mapView
         
+       // getCurrentLocation()
         
-//        let polyLine: GMSPolyline = GMSPolyline(path: path)
-//        polyLine.isTappable = true
-//        polyline.zIndex = 0
-//        polyline.map = mapView
     }
+     
+    
+    
+    
+     
+
 
     func getCurrentLocation() {
         // Ask for Authorisation from the User.
